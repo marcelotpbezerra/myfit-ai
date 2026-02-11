47b47270bb961accf0bd2ec2acb14b46779314cd
Marcelo Bezerra <seta.mbezerra@gmail.com>
2026-02-10 23:26:32 -0300

    fix: force webpack build and disable middleware temp

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
     eslint: { ignoreDuringBuilds: true },
     typescript: { ignoreBuildErrors: true },
+    images: { unoptimized: true },
 };
 
 export default nextConfig;
